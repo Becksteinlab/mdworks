963993ec02691e0e4f1c0633f6425b10aa64cff9
David Dotson <dldotson@asu.edu>
2016-04-25 17:35:53 -0700

    Still hacking.

--- firetasks.py
+++ firetasks.py
@@ -8,19 +8,7 @@ class GMXmdrunTask(FireTaskBase):
     optional_params = ["modules"]
 
     def run_task(self, fw_spec):
-        smaller = self['smaller']
-        larger = self['larger']
 
-        m_sum = smaller + larger
-        if m_sum < stop_point:
-            print('The next Fibonacci number is: {}'.format(m_sum))
-            # create a new Fibonacci Adder to add to the workflow
-            new_fw = Firework(GMXmdrunTask(), {'smaller': larger, 'larger': m_sum, 'stop_point': stop_point})
-            return FWAction(stored_data={'next_fibnum': m_sum}, additions=new_fw)
-
-        else:
-            print('We have now exceeded our limit; (the next Fibonacci number would have been: {})'.format(m_sum))
-            return FWAction()
 
 
 class FileRsyncTask(FireTaskBase):
