bbf51ef354f9dee8a3ecbd27979ef8e22633620e
David Dotson <dldotson@asu.edu>
2016-04-26 21:03:19 -0700

    Hacking away on new plan for flexible molecular dynamics.
    Almost there. Need a BeaconTask yet.

--- firetasks.py
+++ firetasks.py
@@ -13,6 +13,59 @@ class GMXmdrunTask(FireTaskBase):
         pass
 
 
+class FilePull(FireTaskBase):
+    """
+    A FireTask to Transfer files from a remote server. Note that
+    Required params:
+        - files: ([str]) - list of source files or directories
+        - dest: (str) destination directory, if not specified within files parameter
+    Optional params:
+        - server: (str) server host for source files
+        - user: (str) user to authenticate with on remote server
+        - key_filename: (str) optional SSH key location for remote transfer
+    """
+    _fw_name = 'PullTask'
+    required_params = ["files"]
+
+    def run_task(self, fw_spec):
+        shell_interpret = self.get('shell_interpret', True)
+        ignore_errors = self.get('ignore_errors')
+
+        # remote transfers
+        # Create SFTP connection
+        import paramiko
+        ssh = paramiko.SSHClient()
+        ssh.load_host_keys(expanduser(os.path.join("~", ".ssh", "known_hosts")))
+        ssh.connect(self['server'], username=self.get('user'), key_filename=self.get['key_filename'])
+        sftp = ssh.open_sftp()
+
+        for src in self["files"]:
+            try:
+                dest = self['dest']
+
+                # make destination if it doesn't exist already
+                if not os.path.exists(dest):
+                    os.mkdirs(dest)
+
+                # try case where src is a directory
+                try:
+                    for g in sftp.listdir(src):
+                        sftp.get(os.path.join(src, g), os.path.join(dest, g))
+                except IOError:
+                    # if src isn't a directory, it should be a file
+                    sftp.get(src, os.path.join(dest, os.path.basename(src)))
+
+            except:
+                traceback.print_exc()
+                if not ignore_errors:
+                    raise ValueError(
+                        "There was an error performing operation {} from {} "
+                        "to {}".format(mode, self["files"], self["dest"]))
+
+        sftp.close()
+        ssh.close()
+
+
 class FileRsyncTask(FireTaskBase):
     """
     A FireTask to transfer files with rsync. Note that
--- interactive.py
+++ interactive.py
@@ -1,29 +1,26 @@
 import os
 
-from fireworks import ScriptTask, PyTask
+from fireworks import ScriptTask, PyTask, FileTransferTask
 from fireworks.core.firework import Firework
 
 from fireworks.features.background_task import BackgroundTask
 
-from .firetasks import GMXmdrunTask, FileRsyncTask
+from mdworks.firetasks import FilePull
 
-
-def make_md_firework(name, launchdir, scratchdir, deffnm='md', tpr='md.tpr',
-                     cpt='md.cpt', srcuser=None, srchost=None):
-    """Construct an MD Firework. This firework can then be incorporated into a
-    workflow and submitted to a launchpad.
+def make_md_workflow(sim, archive, stages, deffnm='md', tpr='md.tpr', cpt='md.cpt'):
+    """Construct an MD workflow.
 
     Parameters
     ----------
-    name : str
-        Name to give this Firework.
-    launchdir : str
-        Absolute path to directory to launch from, which holds all required and
-        generated data.
-    scratchdir : str
-        Absolute path to directory on remote resource to run MD within.
-    deffnm : str
-        Prefix for all Gromacs output files.
+    sim : str
+        MDSynthesis Sim.
+    archive : str
+        Absolute path to directory to launch from, which holds all required data.
+    stages: list
+        Dicts giving for each of the following keys:
+            - 'server': server host to transfer to
+            - 'user': username to authenticate with
+            - 'staging': absolute path to staging area on remote resource
     tpr : str
         File name (not path) of run-input file.
     cpt : str
@@ -32,56 +29,65 @@ def make_md_firework(name, launchdir, scratchdir, deffnm='md', tpr='md.tpr',
     Returns
     -------
     firework 
-        MD firework; can be submitted as part of a workflow to LaunchPad of
-        choice.
+        MD workflow; can be submitted to LaunchPad of choice.
 
     """
-    tpr = os.path.join(launchdir, tpr)
-    cpt = os.path.join(launchdir, cpt)
-
     if os.path.exists(cpt):
         files = [tpr, cpt]
     else:
         files = [tpr]
 
-    # make scratch dir
-    ft_makedir = PyTask(func='os.makedirs', args=[scratchdir])
+    ## Stage files on all resources where MD may run; takes place locally
+    fts_stage = list()
+    for stage in stages:
+        fts_stage.append(FileTransferTask(mode='rtransfer',
+                                          server=stage['server'],
+                                          user=stage['user'],
+                                          files=[os.path.join(archive, i) for i in files],
+                                          dest=os.path.join(stage['staging'], sim.uuid),
+                                          shell_interpret=True))
 
-    # rsync needed files
-    if srcuser and srchost:
-        ft_copy = FileRsyncTask(mode='remotesrc',
-                                files=files,
-                                dest=scratchdir,
-                                srcuser=srcuser,
-                                srchost=srchost)
-    else:
-        ft_copy = FileRsyncTask(mode='local',
-                                files=files,
-                                dest=scratchdir)
+    fw_stage = Firework(fts_stage,
+                        spec={'_launch_dir': archive,
+                              '_category': 'local'},
+                        name='staging')
+
+
+    ## MD execution; takes place in queue context of compute resource
+
+    # copy input files to scratch space
+    ft_copy = FileTransferTask(mode='copy',
+                               files=[os.path.join('$STAGING/', sim.uuid, i) for i in files],
+                               dest='$WORK/',
+                               shell_interpret=True)
 
     # next, run MD
-    ft_md = ScriptTask(script='run_md.sh', stdin_key=scratchdir, fizzle_bad_rc=True)
+    ft_md = ScriptTask(script='run_md.sh', stdin_key, fizzle_bad_rc=True)
+
+    # send info on where files live to pull firework
+    ft_info = BeaconTask()
+
+    fw_md = Firework([ft_copy, ft_md],
+                     spec={'_category': 'md',
+                           '_pass_job_info': True},
+                     name='md',
+                     parents=fw_stage)
+
  
-    # finally, copy back what's left
-    if srcuser and srchost:
-        ft_copyback = FileRsyncTask(mode='remotedest',
+    ## Pull files back to archive; takes place locally
+    ft_copyback = FilePull(files=[],                        # updated by previous FW
+                           dest=archive,
+                           server=None,                     # updated by previous FW
+                           spec={'_launch_dir': archive,
+                                 '_category': 'local'},
+                           name='pull',
+                           parents=fw_md)
+
+
+    ft_copyback = FileRsyncTask(mode='remotedest',
                                     files='{}/*'.format(scratchdir),
                                     dest=launchdir,
                                     destuser=srcuser,
                                     desthost=srchost)
-    else:
-        ft_copyback = FileRsyncTask(mode='local',
-                                    files='{}/*'.format(scratchdir),
-                                    dest=launchdir)
     
-    # periodically pull the latest files in the background
-    bg1 = BackgroundTask(ft_copyback, sleep_time=3600, run_on_finish=True)
-    
-    fw_md = Firework([ft_makedir, ft_copy, ft_md], 
-                     spec={'_launch_dir': launchdir,
-                           '_category': 'md',
-                           '_background_tasks': [bg1],
-                           '_queueadapter': {'launch_dir': scratchdir}},
-                     name=name)
-
-    return fw_md
+    return wf
